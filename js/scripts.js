dbb4a0245748fb30c8dc816faed6eef0e3b05385
William Kulha <kulha.william@gmail.com>
2018-09-10 15:15:39 -0400

    Add javascript to take in customers input and return a success screen with their info given back to them.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,18 @@ $(document).ready(function(){
   $("form#billinginfo").submit(function(event){
     event.preventDefault();
     var customerName = $("input#fullName").val();
-    var streetaddress = $("input#streetadress").val();
+    var streetAddress = $("input#streetaddress").val();
+    var city = $("input#city").val();
+    var zipCode = $("input#zipcode").val();
     var state = $("#state").val();
-    var country = $("#state").val();
+    var country = $("#country").val();
+    var customerEmail = $("input#emailaddress").val();
+    $(".customerName").text(customerName);
+    $(".streetAddress").text(streetAddress);
+    $(".customerCity").text(city);
+    $(".customerState").text(state);
+    $(".customerZipCode").text(zipCode);
+    $(".customerCountry").text(country);
+    $(".customerEmail").text(customerEmail);
   });
 });
